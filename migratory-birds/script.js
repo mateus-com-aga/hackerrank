97088e2a4841e524176d74ef5442648c69e0da0e
matheusFarias <senxdx.farias@gmail.com>
2024-11-13 16:46:44 -0300

    Birds challenge

---
+++ migratory-birds/script.js
@@ -0,0 +1,16 @@
+/*
+ * Complete the 'migratoryBirds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function migratoryBirds(arr) {
+  // Write your code here
+
+  console.table(arr)
+
+
+}
+
+migratoryBirds([1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4])
